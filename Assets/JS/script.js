ff7404413cadf91b3e7475edb49f814c721ef176
John Dudley <dudleyinc3@gmail.com>
2016-04-16 12:02:53 -0700

    main code structure

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,23 @@
-alert("hey");
+//Check off Todos' with click
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text input
+		var todoText = $(this).val();
+		//clear text input box
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span>X </span>" + todoText + "</li>")
+	}
+});
